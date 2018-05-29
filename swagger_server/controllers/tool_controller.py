59ea9eca6730278bd9c92de4ccf420ba5c7489a3
Daesub Kim <rano@bluehack.net>
2018-05-29 16:23:39 +0900

    Resolved [AMZ-25]: Project setup

---
+++ swagger_server/controllers/tool_controller.py
@@ -0,0 +1,90 @@
+import connexion
+import six
+
+from swagger_server.models.get_dictionary_words_filtered_response import GetDictionaryWordsFilteredResponse  # noqa: E501
+from swagger_server.models.get_dictionary_words_response import GetDictionaryWordsResponse  # noqa: E501
+from swagger_server.models.get_tool_titles_response import GetToolTitlesResponse  # noqa: E501
+from swagger_server.models.post_dictionary_products_attrs_sub_attrs_response import PostDictionaryProductsAttrsSubAttrsResponse  # noqa: E501
+from swagger_server.models.post_dictionary_words_response import PostDictionaryWordsResponse  # noqa: E501
+from swagger_server import util
+
+
+def get_dictionary_words(nodeId):  # noqa: E501
+    """Get dictionary words for filtering within a specified browse node
+
+    Return Dictionary Words for filtering ingredients # noqa: E501
+
+    :param nodeId: 
+    :type nodeId: str
+
+    :rtype: GetDictionaryWordsResponse
+    """
+    return 'do some magic!'
+
+
+def get_dictionary_words_filtered(nodeId, filters):  # noqa: E501
+    """Get filtered words within a specified browse node with filtering words
+
+    Return filtered words cloud # noqa: E501
+
+    :param nodeId: 
+    :type nodeId: str
+    :param filters: 
+    :type filters: List[str]
+
+    :rtype: GetDictionaryWordsFilteredResponse
+    """
+    return 'do some magic!'
+
+
+def get_tool_titles(nodeId):  # noqa: E501
+    """Get 100 amazon best selling item titles within a specified browse node
+
+    Return Titles # noqa: E501
+
+    :param nodeId: 
+    :type nodeId: str
+
+    :rtype: GetToolTitlesResponse
+    """
+    return 'do some magic!'
+
+
+def post_dictionary_products_attrs_sub_attrs(nodeId, attrId, subAttrId, subAttrUsName, subAttrKrName, attrUsName=None, attrKrName=None):  # noqa: E501
+    """Add sub attribute within a specified browse node&#39;s attribute
+
+    Add sub attribute # noqa: E501
+
+    :param nodeId: 
+    :type nodeId: str
+    :param attrId: 
+    :type attrId: str
+    :param subAttrId: 
+    :type subAttrId: str
+    :param subAttrUsName: 
+    :type subAttrUsName: str
+    :param subAttrKrName: 
+    :type subAttrKrName: str
+    :param attrUsName: Upserting if exists
+    :type attrUsName: str
+    :param attrKrName: Upserting if exists
+    :type attrKrName: str
+
+    :rtype: PostDictionaryProductsAttrsSubAttrsResponse
+    """
+    return 'do some magic!'
+
+
+def post_dictionary_words(subAttrId, word):  # noqa: E501
+    """Add filtering word to dictionary
+
+    Add to dictionary # noqa: E501
+
+    :param subAttrId: 
+    :type subAttrId: str
+    :param word: 
+    :type word: str
+
+    :rtype: PostDictionaryWordsResponse
+    """
+    return 'do some magic!'
---
+++ swagger_server/test/test_tool_controller.py
@@ -0,0 +1,88 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.get_dictionary_words_filtered_response import GetDictionaryWordsFilteredResponse  # noqa: E501
+from swagger_server.models.get_dictionary_words_response import GetDictionaryWordsResponse  # noqa: E501
+from swagger_server.models.get_tool_titles_response import GetToolTitlesResponse  # noqa: E501
+from swagger_server.models.post_dictionary_products_attrs_sub_attrs_response import PostDictionaryProductsAttrsSubAttrsResponse  # noqa: E501
+from swagger_server.models.post_dictionary_words_response import PostDictionaryWordsResponse  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestToolController(BaseTestCase):
+    """ToolController integration test stubs"""
+
+    def test_get_dictionary_words(self):
+        """Test case for get_dictionary_words
+
+        Get dictionary words for filtering within a specified browse node
+        """
+        response = self.client.open(
+            '//tool/dictionary/words/{nodeId}'.format(nodeId='nodeId_example'),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_dictionary_words_filtered(self):
+        """Test case for get_dictionary_words_filtered
+
+        Get filtered words within a specified browse node with filtering words
+        """
+        query_string = [('filters', 'filters_example')]
+        response = self.client.open(
+            '//tool/dictionary/words/{nodeId}/filtered'.format(nodeId='nodeId_example'),
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_tool_titles(self):
+        """Test case for get_tool_titles
+
+        Get 100 amazon best selling item titles within a specified browse node
+        """
+        response = self.client.open(
+            '//tool/titles/{nodeId}'.format(nodeId='nodeId_example'),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_post_dictionary_products_attrs_sub_attrs(self):
+        """Test case for post_dictionary_products_attrs_sub_attrs
+
+        Add sub attribute within a specified browse node's attribute
+        """
+        query_string = [('attrUsName', 'attrUsName_example'),
+                        ('attrKrName', 'attrKrName_example'),
+                        ('subAttrId', 'subAttrId_example'),
+                        ('subAttrUsName', 'subAttrUsName_example'),
+                        ('subAttrKrName', 'subAttrKrName_example')]
+        response = self.client.open(
+            '//tool/dictionary/products/{nodeId}/attrs/{attrId}/subAttrs'.format(nodeId='nodeId_example', attrId='attrId_example'),
+            method='POST',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_post_dictionary_words(self):
+        """Test case for post_dictionary_words
+
+        Add filtering word to dictionary
+        """
+        query_string = [('subAttrId', 'subAttrId_example'),
+                        ('word', 'word_example')]
+        response = self.client.open(
+            '//tool/dictionary/words',
+            method='POST',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
